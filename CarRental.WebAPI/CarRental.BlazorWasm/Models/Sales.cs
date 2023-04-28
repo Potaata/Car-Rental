6bb93dfa3dab8485037463cb36b530bb05f08b86
Baibhab Bhattarai <baibhab.bhattarai.2001@gmail.com>
2023-04-28 12:29:26 +0545

    [Client]: Staff and Sales Model

---
+++ CarRental.WebAPI/CarRental.BlazorWasm/Models/Sales.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRental.BlazorWasm.Models
+{
+    public class Sales
+    {
+        public string CustomerName { get; set; }
+        public string CarName { get; set; }
+        public DateTime RentedOn { get; set; }
+        public string AuthorizedBy { get; set; }
+
+    }
+}
