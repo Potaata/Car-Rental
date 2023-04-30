d0a1b07ad256d84fe24cd95c5c054abb641e5b40
Baibhab Bhattarai <baibhab.bhattarai.2001@gmail.com>
2023-04-30 13:43:20 +0545

    [Client]: DamageService Class

---
+++ CarRental.WebAPI/CarRental.BlazorWasm/Services/ItemService/DamageService.cs
@@ -0,0 +1,49 @@
+﻿using CarRental.BlazorWasm.Models;
+using CarRental.BlazorWasm.Models.Damages;
+using CarRental.BlazorWasm.Models.Items;
+using CarRental.BlazorWasm.Models.Staffs;
+
+namespace CarRental.BlazorWasm.Services.ItemService
+{
+    public class DamageService : IItemService<Damage, DamageRequest>
+    {
+        private readonly ApiService _apiService;
+
+        public string EndPoint = "/api/damages";
+
+
+        public DamageService(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<List<Damage>> GetItems()
+        {
+            throw new Exception("We don't need this here");
+        }
+
+        public async Task<string> DeleteItem(int id)
+        {
+            throw new Exception("We don't need this here");
+
+        }
+        public async Task<string> CreateItem(DamageRequest staff)
+        {
+            throw new Exception("We don't need this here");
+        }
+
+        public async Task<string> EditItem(int id, DamageRequest damage)
+        {
+            MessageResponse message = await _apiService.PUT<DamageRequest, MessageResponse>(EndPoint + '/' + id, damage);
+
+            return message.message;
+        }
+
+        public DamageRequest GetDefaultRequest()
+        {
+            return new DamageRequest();
+        }
+    }
+
+
+}
